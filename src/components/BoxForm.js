2d70ec47d7bb90f552abbb26f00b9afe35a44efb
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-06 20:44:59 -0500

    Add useState initial logic

--- src/components/BoxForm.js
+++ src/components/BoxForm.js
@@ -1,13 +1,18 @@
 import { useState } from "react";
 
 const BoxForm = (props) => {
+	const [color, setColor] = useState("")
 
-
+	const handleSubmit = (e) => {
+		e.preventDefault()
+	}
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<label htmlFor="color">Color</label>
-			<input type="text" name="color" placeholder="Choose a color" value={value} />
-			<button>Add</button>
+			<input type="text" name="color" placeholder="Choose a color" onChange={(e) => setColor(e.target.value)} value={color} />
+			<button type="submit">Add</button>
 		</form>
 	)
 }
+
+export default BoxForm;
