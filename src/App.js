c7875b9510bed85fa91e273da919cbddc6a9a937
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-06 21:05:45 -0500

    Define App useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React, { useState } from 'react';
 import './App.css';
+import BoxForm from './components/BoxForm';
+import BoxDisplay from './components/BoxDisplay';
 
 function App() {
+  const [currentColors, setCurrentColors] = useState("");
+
+  const updateColorList = (newColors) => {
+    setCurrentColors(newColors)
+  }
+
   return (
     <div className="App">
-      
+      <BoxForm onNewColors={updateColorList} />
+      <BoxDisplay displayedColors={currentColors} />
     </div>
   );
 }
